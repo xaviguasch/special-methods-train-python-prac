dc3da3a291357077bfaa82fa7f091c4e5a12f79c
xaviguasch <xaviguasch@gmail.com>
2021-01-26 18:51:32 +0100

    added finished exercises

---
+++ main.py
@@ -0,0 +1,14 @@
+class Train:
+    def __init__(self, num_cars):
+        self.num_cars = num_cars
+
+    def __repr__(self):
+        return "{} car train".format(self.num_cars)
+
+    def __len__(self):
+        return self.num_cars
+
+
+n1 = Train(10)
+print(n1)
+print(len(n1))
